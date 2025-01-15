fdfde48564a3eafedfb247a9ca5fe0bc09783cc4
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-15 11:22:56 +0200

    used the correct key for id

--- Controller/userController.js
+++ Controller/userController.js
@@ -49,13 +49,13 @@ exports.updateMe = CatchAsync(async (req, res, next) => {
 });
 
 exports.getMe = (req, res, next) => {
-  req.params.id = req.user.id;
+  req.params.id = req.user._id;
 
   next();
 };
 
 exports.getUser = CatchAsync(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params._id);
 
   if (!user) {
     return next(new AppError("No document found with that ID"), 404);
