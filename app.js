958fd5a372aa7304f307c9d885c6b1d653a9e40a
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-17 21:46:33 +0200

    removed the enabling of preflight requests

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-app.options("*", cors());
+//app.options("*", cors());
 app.get("/", (req, res) => {
   res
     .status(200)
