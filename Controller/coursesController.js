ea782772efb55d73e56fcd26a466f85f1c3f01dd
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-25 00:30:01 +0200

    added the courses countroller that reference data to the user

--- Controller/coursesController.js
+++ Controller/coursesController.js
@@ -1,5 +1,22 @@
 const CatchAsync = require("../utils/CatchAsync");
 const course = require("../Models/courseModel");
+exports.createCourse = CatchAsync(async (req, res, next) => {
+  const newCourse = await Courses.create(req.body);
+  const savedCourse = await newCourse.save();
+
+  await User.findByIdAndUpdate(req.user.id, {
+    $push: {
+      courses: savedCourse._id,
+    },
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      newCourse,
+    },
+  });
+});
 
 exports.getAllCourses = CatchAsync(async (req, res, next) => {
   const courses = await course.find();
