8c4b51d07ed16219b62dc9e8ad563e10a20b2f39
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-24 20:51:05 +0200

    added the routes to all CRUD functions

--- routes/coursesRoutes.js
+++ routes/coursesRoutes.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const {
   getAllCourses,
+  updateCourse, // Ensure the correct function name is used
+  getOneCourse,
+  deleteCourse,
   createCourse,
 } = require("../Controller/coursesController");
-
+const { protect } = require("../Controller/authController");
 const router = express.Router();
 
+router.post("/addCourse", protect, createCourse);
 router.get("/getAllCourses", getAllCourses);
-router.post("/addCourse", createCourse);
+router.patch("/updateCourse/:id", protect, updateCourse); // Ensure the correct function name is used
+router.get("/getCourse/:id", protect, getOneCourse);
+router.delete("/deleteCourse/:id", protect, deleteCourse);
 
 module.exports = router;
